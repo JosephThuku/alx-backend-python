fc63d57d8fb1b905eb0f8be6d6caaf29fcdafca4
JosephThuku <jthuku490@gmail.com>
2023-07-11 12:20:44 +0300

    task 3 and 4

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,7 +6,9 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """measure runtime"""
+    """
+    measure runtime of our function
+    """
     start_time = time.time()
     run(wait_n(n, max_delay))
     end_time = time.time()
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,9 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """uses task_wait_random"""
+    """
+    uses task_wait_random
+    """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     results = await asyncio.gather(*tasks)
     return nsmallest(n, results)
