aecd32a10b745cef3c5f27dd9ea71c175c4a1ea3
JosephThuku <jthuku490@gmail.com>
2023-07-12 03:18:29 +0300

    debugged all files to meet recquirements

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-""" async comprehension """
-import asyncio
-import random
+"""asynch generator"""
+
+
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
-    """ asnc comprehension dfunction """
-    random_numbers = [number async for number in async_generator()]
-    return random_numbers
+async def async_comprehension() -> List[float]:
+    """Return the 10 random numbers"""
+    results = [i async for i in async_generator()]
+    return results
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
-"""
-measure_runtime coroutine
-This will execute async_comprehension four times
-in parallel using asyncio.gather.
-"""
-import asyncio, random
+"""measuring runtime"""
+
+
+import time
+import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
-    """ measure the runtime """
-    start_time = asyncio.get_event_loop().time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-    end_time = asyncio.get_event_loop().time()
+async def measure_runtime() -> float:
+    """Measure the total runtime"""
+    start_time = time.time()
+
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+
+    end_time = time.time()
     return end_time - start_time
